d9472f605becfc46a57de41cabbca718b13ae136
Nikola Vukovic <nikolav54@gmail.com>
2021-10-26 14:31:52 +0200

    Added delete functionality

--- src/DemoApp.jsx
+++ src/DemoApp.jsx
@@ -25,60 +25,78 @@ const DemoApp = () => {
 
   const [scheduledEventsArray, setScheduledEventsArray] =
     useState(scheduledEvents);
+  const [currentEvent, setCurrentEvent] = useState({});
 
   const handleDateClick = (event) => {
     console.log(`dateClick`, event);
     let title = window.prompt("Unesite ime Event-a: ");
+    console.log(`title: `, title);
 
-    scheduledEvents.push(
-      event.hasOwnProperty("dateStr")
-        ? {
-            title,
-            date: event.dateStr,
-            allDay: event.allDay,
-          }
-        : {
-            title,
-            date: event.hasOwnProperty("dateStr")
-              ? event.dateStr
-              : event.hasOwnProperty("startStr")
-              ? event.startStr
-              : null,
-            end: event.hasOwnProperty("endStr") ? event.endStr : null,
-            allDay: event.hasOwnProperty("startStr") ? false : event.allDay,
-          }
-    );
-    setScheduledEventsArray(scheduledEvents);
-    localStorage.setItem(
-      "events",
-      encodeURI(JSON.stringify(scheduledEventsArray))
-    );
-    console.log(scheduledEventsArray);
+    if (title && title.trim().length !== 0) {
+      scheduledEvents.push(
+        event.hasOwnProperty("dateStr")
+          ? {
+              title,
+              date: event.dateStr,
+              allDay: event.allDay,
+            }
+          : {
+              title,
+              date: event.hasOwnProperty("dateStr")
+                ? event.dateStr
+                : event.hasOwnProperty("startStr")
+                ? event.startStr
+                : null,
+              end: event.hasOwnProperty("endStr") ? event.endStr : null,
+              allDay: event.hasOwnProperty("startStr") ? false : event.allDay,
+            }
+      );
+      setScheduledEventsArray(scheduledEvents);
+      localStorage.setItem(
+        "events",
+        encodeURI(JSON.stringify(scheduledEventsArray))
+      );
+      console.log(scheduledEventsArray);
+    }
   };
 
   const select = (event) => {
     console.log(`eventClick`, event);
-    scheduledEvents.push({
-      title: "event 3",
-      start: event.hasOwnProperty("dateStr")
-        ? event.dateStr
-        : event.hasOwnProperty("startStr")
-        ? event.startStr
-        : null,
-      end: event.hasOwnProperty("endStr") ? event.endStr : null,
-      allDay: event.hasOwnProperty("startStr") ? false : event.allDay,
-    });
-    setScheduledEventsArray(scheduledEvents);
-    localStorage.setItem(
-      "events",
-      encodeURI(JSON.stringify(scheduledEventsArray))
-    );
+    // scheduledEvents.push({
+    //   title: "event 3",
+    //   start: event.hasOwnProperty("dateStr")
+    //     ? event.dateStr
+    //     : event.hasOwnProperty("startStr")
+    //     ? event.startStr
+    //     : null,
+    //   end: event.hasOwnProperty("endStr") ? event.endStr : null,
+    //   allDay: event.hasOwnProperty("startStr") ? false : event.allDay,
+    // });
+    // setScheduledEventsArray(scheduledEvents);
+    // localStorage.setItem(
+    //   "events",
+    //   encodeURI(JSON.stringify(scheduledEventsArray))
+    // );
     console.log(scheduledEventsArray);
   };
 
   const editEvent = () => {};
 
-  const deleteEvent = () => {};
+  const deleteEvent = () => {
+    let events = JSON.parse(decodeURI(localStorage.getItem("events")));
+    if (!events) events = scheduledEventsArray;
+    let eventTitles = events.map((e) => e.title);
+    console.log(`currentEvent`, currentEvent);
+    console.log("deleteEvent", eventTitles.indexOf(currentEvent));
+
+    const index = events.map((event) => event.title).indexOf(currentEvent);
+    if (index > -1) {
+      events.splice(index, 1);
+      setScheduledEventsArray(events);
+      localStorage.setItem("events", encodeURI(JSON.stringify(events)));
+      console.log(events);
+    }
+  };
 
   return (
     <>
@@ -97,6 +115,7 @@ const DemoApp = () => {
             info.jsEvent.preventDefault(); // don't let the browser navigate
             console.log(`info`, info);
             show();
+            setCurrentEvent(info.event._def.title);
             document.documentElement.scrollTop = 0;
 
             if (info.event.url) {
@@ -140,11 +159,7 @@ const DemoApp = () => {
             <button className="btn" onClick={editEvent} type="button">
               Izmjeni
             </button>
-            <button
-              className="btn"
-              onClick={(event) => deleteEvent(event)}
-              type="button"
-            >
+            <button className="btn" onClick={deleteEvent} type="button">
               Obrisi
             </button>
           </div>
